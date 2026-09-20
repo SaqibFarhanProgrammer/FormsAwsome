c805c953ddb72fa066b29593a6a276c395c90ea9
SaqibFarhanProgrammer <saqibkhatai@gmail.com>
2026-09-20 21:34:05 +0500

    refactor: clean up AnalyticsClient component by removing commented sections

--- features/anylatics/components/AnalyticsClient.tsx
+++ features/anylatics/components/AnalyticsClient.tsx
@@ -23,16 +23,13 @@ const ViewsChart = dynamic(() => import("./ViewsChart").then((module) => module.
 export function AnalyticsClient() {
   return (
     <div className="space-y-6">
-      {/* KPI Overview Cards */}
       <AnalyticsOverview />
 
-      {/* Main Charts Row */}
       <div className="grid gap-6 lg:grid-cols-3">
         <SubmissionsChart />
         <FormsPerformanceChart />
       </div>
 
-      {/* Secondary Charts Row */}
       <div className="grid gap-6 lg:grid-cols-2">
         <DeviceChart />
         <ViewsChart />
